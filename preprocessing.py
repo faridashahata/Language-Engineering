6a4da1cb748b991635fce5fd23ca98c05fb56ea1
katja <katja.roos@leanon.se>
2023-05-26 13:19:55 +0200

    Clean the code and change threshold

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,9 @@
-import pandas as pd
-import os
 import re
 
 import pandas as pd
 import torch
 from torch.utils.data import (TensorDataset)
-from tqdm import tqdm
-# from pytorch_transformers import AdamW, WarmupLinearSchedule, T5Tokenizer
 from transformers import T5Tokenizer
-
 
 train_df = pd.read_json("./data/train.jsonl", lines=True)
 test_df = pd.read_json("./data/test.jsonl", lines=True)
@@ -28,14 +23,12 @@ print("The shape of the training dataframe: ", train_df.shape)
      Add "</s>" to end of summary and document  """
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 global MODEL_NAME
-MODEL_NAME = 't5-small'
+MODEL_NAME = 't5-large'
 global BATCH_SIZE
 BATCH_SIZE = 64
 
 
-
 def prepare_data(df, threshold, tokenizer):
-
     # Lowercase the data:
     df['document'] = df['document'].apply(lambda x: x.lower())
     df['summary'] = df['summary'].apply(lambda x: x.lower())
@@ -51,93 +44,31 @@ def prepare_data(df, threshold, tokenizer):
     df['document'] = df['document'].apply(lambda x: " ".join(x.split()))
     df['summary'] = df['summary'].apply(lambda x: " ".join(x.split()))
 
-
-
     # Generate word counts:
     df['document_word_count'] = df['document'].apply(lambda x: len(x.split()))
     df['summary_word_count'] = df['summary'].apply(lambda x: len(x.split()))
 
-
-    #df['summary_token_count'] = df['summary'].apply(lambda x: len(tokenizer.tokenize(x)))
+    # df['summary_token_count'] = df['summary'].apply(lambda x: len(tokenizer.tokenize(x)))
 
     # Add "summarize" before document:
     df['document'] = 'summarize: ' + df['document']
-
     # Add pad token to summaries:
     df['summary'] = '<pad>' + df['summary']
-
-    # Add " </s>" to end of each document and summary:
-    # df['document'] = df['document'] + " </s>"
-    # df['summary'] = df['summary'] + " </s>"
-
-
     # Truncate data:
     new_df = df[df.document_word_count <= threshold].copy()
-
     # Generate token counts:
     new_df['doc_token_count'] = new_df['document'].apply(lambda x: len(tokenizer.tokenize(x)))
 
-    #Truncate based on tokens:
-    new_df = new_df[new_df.doc_token_count <= 1.5*threshold]
-
+    # Truncate based on tokens:
+    new_df = new_df[new_df.doc_token_count <= 1.5 * threshold]
 
     # Remove rows with no summary or document:
     new_df = new_df[new_df.document_word_count > 0]
     new_df = new_df[new_df.summary_word_count > 0]
-    
+
     return new_df
-# MODEL_NAME = 't5-small'
 
 tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
-train_df = prepare_data(train_df, 200, tokenizer)
-val_df = prepare_data(val_df, 200, tokenizer)
-test_df = prepare_data(test_df, 200, tokenizer)
-
-# plt.hist(train_df['document_word_count'])
-# plt.show()
-
-print("len of train dataframe", len(train_df))
-
-# # Mean Girls example, after clean-up:
-# print("Cleaned sample document: ", train_df.iloc[33614].document)
-# print("Cleaned sample summary: ", train_df.iloc[33614].summary)
-# print("Summary word count: ", train_df.iloc[33614].summary_word_count)
-# print("Document word count: ", train_df.iloc[33614].document_word_count)
-#
-# print("Average Summary word count: ", train_df.summary_word_count.mean())
-# print("Average Document word count: ", train_df.document_word_count.mean())
-#
-print("Max Summary word count: ", train_df.summary_word_count.max())
-
-# print("document with max doc word count: ", train_df[train_df.document_word_count == train_df.document_word_count.max()].iloc[0].document)
-# print("summary with max doc word count: ", train_df[train_df.document_word_count == train_df.document_word_count.max()].iloc[0].summary)
-
-
-print("Max Document word count: ", train_df.document_word_count.max())
-
-# Following this guide closely: http://seekinginference.com/applied_nlp/T5.html
-
-
-#plt.hist(train_df[train_df.document_word_count <1000].summary_word_count)
-
-#plt.hist(train_df.document_word_count)
-#plt.show()
-
-
-# print("entries with document length > 1000", train_df[train_df.document_word_count>=1000].shape[0])
-#
-
-
-
-
-# STEP 2: INSTANTIATE T5 TOKENZIER and TOKENIZE THE DATA:
-
-
-# tokenizer = T5Tokenizer.from_pretrained('t5-small')
-#
-# print("eos token id: ", tokenizer.eos_token_id)
-# print("unk token id: ", tokenizer.unk_token_id)
-# print("pad token id: ", tokenizer.pad_token_id)
 
 """ 
 The output of tokenizer is a dictionary containing two keys – input ids and attention mask. 
@@ -146,83 +77,37 @@ Attention mask is used to batch the input sequence together and indicate whether
 should be attended by our model or not.Token with attention mask value 0 means token will 
 be ignored and 1 means tokens are important and will be taken for further processing.
 Source: https://neptune.ai/blog/hugging-face-pre-trained-models-find-the-best#:~:text=The%20output%20of%20tokenizer%20is,by%20our%20model%20or%20not.
-
 """
-
 def tokenize(df, tokenizer, max_len):
     input_ids = []
     attention_masks = []
 
     for document in df:
-
         encoded_dict = tokenizer.encode_plus(
-                        document,
-                        add_special_tokens=True,
-                        max_length=max_len,
-                        truncation=True,
-                        padding='max_length',
-                        return_attention_mask=True,
-                        return_tensors='pt'
-                        )
+                document,
+                add_special_tokens=True,
+                max_length=max_len,
+                truncation=True,
+                padding='max_length',
+                return_attention_mask=True,
+                return_tensors='pt'
+        )
         input_ids.append(encoded_dict['input_ids'])
 
         attention_masks.append(encoded_dict['attention_mask'])
-    # print("len of input_ids", len(input_ids))
-    # print("len of input_ids[0]", input_ids[0].size())
-    # print("len of input_ids[0]", input_ids[1].size())
+
     return torch.cat(input_ids, dim=0), torch.cat(attention_masks, dim=0)
 
-
-# # Get max length for tokenized documents:
-#
-token_len = []
-for i in tqdm(range(train_df.shape[0])):
-    if len(tokenizer.tokenize(train_df.iloc[i]['document'])) > 400:
-        print("LARGE TOKENIZED DOC: ", train_df.iloc[i]['document'])
-        print("LARGE DOC AFTER TOKENIZING: ", tokenizer.tokenize(train_df.iloc[i]['document']))
-
-    token_len.append(len(tokenizer.tokenize(train_df.iloc[i]['document'])))
-
-doc_df = pd.DataFrame({"len_tokens": token_len})
-doc_max_len = doc_df['len_tokens'].max()
-
-print("max length of tokenized documents: ", doc_max_len)
-# max is --1868
-
-# Get max length for tokenized summaries:
-token_len = []
-for i in tqdm(range(train_df.shape[0])):
-    token_len.append(len(tokenizer.tokenize(train_df.iloc[i]['summary'])))
-
-summary_df = pd.DataFrame({"len_tokens": token_len})
-summary_max_len = summary_df['len_tokens'].max()
-
-print("max length of tokenized summaries: ", summary_max_len)
-# # max is 955
-#
-# doc_input_ids, doc_attention_masks = tokenize(train_df['document'].values, tokenizer, 1859)
-# summary_input_ids, summary_attention_masks = tokenize(train_df['summary'].values, tokenizer, 907)
-
-
-#
-# PREPARE TENSOR DATASETS: TRAIN, TEST, VAL:
-
 def prepare_dataset(df, tokenizer, threshold):
-
-    #threshold = 1000
+    # threshold = 1000
     df = prepare_data(df, threshold, tokenizer)
 
     DOC_MAX_LEN = int(1.5 * threshold)
-    SUMMARY_MAX_LEN = threshold
     print("DOC_MAX_LEN", DOC_MAX_LEN)
-    print("SUMMARY_MAX_LEN", SUMMARY_MAX_LEN)
+    print("SUMMARY_MAX_LEN", threshold)
     doc_input_ids, doc_attention_masks = tokenize(df['document'].values, tokenizer, DOC_MAX_LEN)
-    summary_input_ids, summary_attention_masks = tokenize(df['summary'].values, tokenizer, SUMMARY_MAX_LEN)
+    summary_input_ids, summary_attention_masks = tokenize(df['summary'].values, tokenizer, threshold)
 
     tensor_df = TensorDataset(doc_input_ids, doc_attention_masks, summary_input_ids, summary_attention_masks)
     return tensor_df
 
-#
-# train_dataset = prepare_dataset(train_df, tokenizer, THRESHOLD)
-# val_dataset = prepare_dataset(val_df, tokenizer, THRESHOLD)
-# test_dataset = prepare_dataset(test_df, tokenizer, THRESHOLD)
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 import numpy as np
@@ -17,8 +18,8 @@ test_df = pd.read_json("./data/test.jsonl", lines=True)
 val_df = pd.read_json("./data/validation.jsonl", lines=True)
 
 # Set global variables:
-EPOCHS = 5
-THRESHOLD: int = 200
+EPOCHS = 10
+THRESHOLD: int = 500
 
 tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
 
@@ -28,6 +29,8 @@ val_dataset = prepare_dataset(val_df, tokenizer, THRESHOLD)
 test_dataset = prepare_dataset(test_df, tokenizer, THRESHOLD)
 
 print("Train data size: ", len(train_dataset))
+print("Val data size: ", len(val_dataset))
+print("Test data size: ", len(test_dataset))
 
 # STEP 1: INSTANTIATE MODEL:
 model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
@@ -58,11 +61,11 @@ class EarlyStopper:
 
 early_stopper = EarlyStopper(patience=3, min_delta=10)
 
-def train(model, batch_size, optimizer, epochs, scheduler, checkpoint_interval, resume_from_checkpoint=None):
+def train(model, batch_size, optimizer, epochs, scheduler, checkpoint_interval, resume_from_checkpoint=None, model_name_pt=None):
     train_stats = []
     val_stats = []
     if resume_from_checkpoint:
-        load_checkpoint(model, optimizer, tokenizer, resume_from_checkpoint)
+        load_checkpoint(model, optimizer, tokenizer, resume_from_checkpoint, model_name_pt)
         start_epoch = int(resume_from_checkpoint.split('_')[-1])
     else:
         start_epoch = 0
@@ -149,8 +152,6 @@ def train(model, batch_size, optimizer, epochs, scheduler, checkpoint_interval, 
         val_stats.append({'Validation Loss': avg_val_loss})
 
         # Add Early Stopping:
-        if early_stopper.early_stop(avg_val_loss):
-            break
 
         print("Summary Results: ")
         print("Epoch | validation Loss")
@@ -170,14 +171,16 @@ def train(model, batch_size, optimizer, epochs, scheduler, checkpoint_interval, 
             best_val_loss = val_stats[epoch]['Validation Loss']
             torch.save(model.state_dict(), f't5_model_{t0}.pt')
 
-            model.save_pretrained(f'./model_save/t5_{t0}/')
-            tokenizer.save_pretrained(f'./model_save/t5_{t0}/')
+            model.save_pretrained(f'./model_save_{MODEL_NAME}_{THRESHOLD}/t5_{t0}/')
+            tokenizer.save_pretrained(f'./model_save_{MODEL_NAME}_{THRESHOLD}/t5_{t0}/')
 
+        if early_stopper.early_stop(avg_val_loss):
+            break
     return train_stats, val_stats
 
 def save_checkpoint(model, optimizer, tokenizer, epoch, val_loss):
     # Create a directory to save the checkpoint
-    checkpoint_dir = f'./checkpoints/epoch_{epoch}'
+    checkpoint_dir = f'./checkpoints_{MODEL_NAME}_{THRESHOLD}/epoch_{epoch}'
     if not os.path.exists(checkpoint_dir):
         os.makedirs(checkpoint_dir)
 
@@ -191,9 +194,9 @@ def save_checkpoint(model, optimizer, tokenizer, epoch, val_loss):
         f.write(f'Epoch: {epoch}\n')
         f.write(f'Validation Loss: {val_loss}\n')
 
-def load_checkpoint(model, optimizer, tokenizer, checkpoint_dir):
+def load_checkpoint(model, optimizer, tokenizer, checkpoint_dir, model_name_pt):
     # Load model state dict
-    model.load_state_dict(torch.load(os.path.join(checkpoint_dir, 'model.pt')))
+    model.load_state_dict(torch.load(os.path.join(checkpoint_dir, model_name_pt)))
     # model.to('mds')
     # Load optimizer state dict
     optimizer.load_state_dict(torch.load(os.path.join(checkpoint_dir, 'optimizer.pt')))
@@ -201,10 +204,14 @@ def load_checkpoint(model, optimizer, tokenizer, checkpoint_dir):
     tokenizer.from_pretrained(checkpoint_dir)
 
 # Set batch size in the global var in preprocessing:
-train(model, BATCH_SIZE, optimizer, EPOCHS, scheduler,  checkpoint_interval=1)
+train_stats, val_stats = train(model, BATCH_SIZE, optimizer, EPOCHS, scheduler,  checkpoint_interval=1)
 
 # Set the path to the checkpoint directory EXAMPLE
 # checkpoint_dir = './checkpoints/epoch_3'
 # Call the train function with the checkpoint
 #train_stats, val_stats = train(model, BATCH_SIZE, optimizer, EPOCHS, scheduler, checkpoint_interval=1, resume_from_checkpoint=checkpoint_dir)
-
+# Save the train and validation stats
+with open(f'checkpoints_{MODEL_NAME}_{THRESHOLD}/train_stats.json', 'w') as f:
+    json.dump(train_stats, f)
+with open(f'checkpoints_{MODEL_NAME}_{THRESHOLD}/val_stats.json', 'w') as f:
+    json.dump(val_stats, f)
