82129481b94c968d2be180f559817b0cb043f2c8
katja <katja.roos@leanon.se>
2023-06-03 16:10:45 +0200

    Adjust testing to look as in the cloud

--- testing.py
+++ testing.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import torch
-import tqdm
+import nltk
+import jsonlines
 from torch.utils.data import (DataLoader)
-# from pytorch_transformers import AdamW, WarmupLinearSchedule, T5Tokenizer
+# from transformers import BartTokenizer, BartForConditionalGeneration
 from transformers import T5Tokenizer, T5ForConditionalGeneration, AdamW
-from transformers import get_linear_schedule_with_warmup
-from train_t5 import MODEL_NAME
-
-
+# from training_barttt import MyDataset
 import preprocessing
 import nlp
 from preprocessing import prepare_dataset
@@ -16,27 +14,25 @@ train_df = pd.read_json("./data/train.jsonl", lines=True)
 test_df = pd.read_json("./data/test.jsonl", lines=True)
 val_df = pd.read_json("./data/validation.jsonl", lines=True)
 
+tokenizer = T5Tokenizer.from_pretrained('t5-base')
 
-device = torch.device("mps")
-
-tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
-
-#Tensor datasets:
-train_dataset = prepare_dataset(train_df, tokenizer, 200)
-val_dataset = prepare_dataset(val_df, tokenizer, 200)
-test_dataset = prepare_dataset(test_df, tokenizer, 200)
+# Tensor datasets:
+train_dataset = prepare_dataset(train_df, tokenizer, 250)
+val_dataset = prepare_dataset(val_df, tokenizer, 250)
+test_dataset = prepare_dataset(test_df, tokenizer, 250)
 
 print("Train data size: ", len(train_dataset))
 print("Val data size: ", len(val_dataset))
 print("Test data size: ", len(test_dataset))
 
-
 # Set up the testing dataloader:
 dataloader = DataLoader(dataset=test_dataset,
-                                shuffle=False,
-                                batch_size=64)
+                        shuffle=False,
+                        batch_size=2)
 
 test_stats = []
+
+
 # Testing loop:
 
 def test(model, dataloader):
@@ -46,26 +42,25 @@ def test(model, dataloader):
     total_test_loss = 0
     predictions = []
     actual_summaries = []
+    docs = []
 
     for step, batch in enumerate(dataloader):
-
-
 
         # Progress update:
         if step % 10 == 0:
             print(f"Batch {step} of a total {len(dataloader)}")
 
         # Unpack training batch from dataloader:
-        doc_input_ids, doc_attention_masks = batch[0], batch[1]
-        summary_input_ids, summary_attention_masks = batch[2], batch[3]
+        doc_input_ids, doc_attention_masks = batch[0].cuda(), batch[1].cuda()
+        summary_input_ids, summary_attention_masks = batch[2].cuda(), batch[3].cuda()
 
         # Forward pass:
         # with autocast():
 
         outputs = model(input_ids=doc_input_ids,
                         attention_mask=doc_attention_masks,
-                        labels=summary_input_ids,
-                        decoder_attention_mask=summary_attention_masks)
+                        labels=summary_input_ids
+                        , decoder_attention_mask=summary_attention_masks)
 
         loss, pred_scores = outputs[:2]
 
@@ -73,21 +68,24 @@ def test(model, dataloader):
         total_test_loss += loss.item()
 
         generated_ids = model.generate(
-            input_ids=doc_input_ids,
-            attention_mask=doc_attention_masks,
-            do_sample=True,
-            temperature=0.8,
-            top_p=0.9,
-            max_length=200,
-            min_length=50,
-            repetition_penalty=2.5
+                input_ids=doc_input_ids,
+                attention_mask=doc_attention_masks,
+                do_sample=True,
+                temperature=0.8,
+                top_p=0.9,
+                max_length=200,
+                min_length=50,
+                repetition_penalty=2.5
 
         )
-
 
         preds = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for g in generated_ids]
 
         target = [tokenizer.decode(t, skip_special_tokens=True, clean_up_tokenization_spaces=True) for t in summary_input_ids]
+        doc = [tokenizer.decode(d, skip_special_tokens=True, clean_up_tokenization_spaces=True) for d in doc_input_ids]
+        print("doc:   ", doc)
+        print("\n")
+        print("\n")
         print("preds:    ", preds)
         print("\n")
         print("target:   ", target)
@@ -95,63 +93,44 @@ def test(model, dataloader):
         print("\n")
         predictions.extend(preds)
         actual_summaries.extend(target)
+        docs.extend(doc)
 
-    avg_test_loss = total_test_loss/len(dataloader)
+    avg_test_loss = total_test_loss / len(dataloader)
     # Record all statistics from this epoch.
     test_stats.append(
-        {
-            'Test Loss': avg_test_loss,
-        }
+            {
+                'Test Loss': avg_test_loss,
+            }
     )
     global test_df
-    #temp_data
-    test_df = pd.DataFrame({'predicted': predictions, 'actual': actual_summaries})
-    #test_df = test_df.append(temp_data)
+    # temp_data
+    test_df = pd.DataFrame({'document': docs, 'predicted': predictions, 'actual': actual_summaries})
+    # test_df = test_df.append(temp_data)
 
     return test_stats
 
 
 # LOAD MODEL:
-model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
-model.load_state_dict(torch.load('t5_model_v2.pt'))
+model = T5ForConditionalGeneration.from_pretrained('t5-base').cuda()
+model.load_state_dict(torch.load('./model_save_t5-base_250_4/t5_model_16:11:52.pt'))
 
 test_stats = test(model, dataloader)
 print("test stats: ", test_stats)
-print("test_df", test_df)
+# print("test_df", test_df)
 print("test_df size", test_df.size)
 print("test loop ran")
-# ROUGE METRICS:
 
-# ROUGE
-nlp_rouge = nlp.load_metric('rouge')
+test_df.to_csv('test_df_t5-base_LAST.csv')
+
+# ROUGE METRICS:
 
 print("rouge metrics loaded")
 import torchmetrics
 from torchmetrics.text.rouge import ROUGEScore
+
 preds = test_df.predicted.to_list()
 target = test_df.actual.to_list()
 rouge = ROUGEScore()
 from pprint import pprint
+
 pprint(rouge(preds, target))
-#
-# scores = nlp_rouge.compute(
-#     test_df.predicted.to_list(), test_df.actual.to_list(),
-#     rouge_types=['rouge1', 'rouge2', 'rougeL', 'rougeLsum'],
-#     use_agregator=True, use_stemmer=False
-# )
-print("scores computed")
-
-#
-# metrics_df = pd.DataFrame({
-#     'rouge1': [scores['rouge1'].mid.precision, scores['rouge1'].mid.recall, scores['rouge1'].mid.fmeasure],
-#     'rouge2': [scores['rouge2'].mid.precision, scores['rouge2'].mid.recall, scores['rouge2'].mid.fmeasure],
-#     'rougeL': [scores['rougeL'].mid.precision, scores['rougeL'].mid.recall, scores['rougeL'].mid.fmeasure]}, index=[ 'P', 'R', 'F'])
-#
-# print("metrics_df built")
-#
-# metrics_df.style.format({'rouge1': "{:.4f}", 'rouge2': "{:.4f}", 'rougeL': "{:.4f}"})
-#
-# print(metrics_df)
-
-
-
